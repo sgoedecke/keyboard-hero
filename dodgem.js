68fa2090d0970086268070ba693ba036ed8becf4
Sean Goedecke <seangoedecke@gmail.com>
2017-11-12 15:09:14 +1100

    implement weird keyboard-matching controls

--- dodgem.js
+++ dodgem.js
@@ -6,14 +6,19 @@ var tick = 0
 var gameLoopInterval
 var WORLD_WIDTH = canvas.width
 var WORLD_HEIGHT = canvas.height
+var MAX_ACCEL = 10
 var world = {
   width: WORLD_WIDTH,
   height: WORLD_HEIGHT,
   player: {
-    x: 250,
-    y: 480,
+    x: WORLD_WIDTH/2,
+    y: WORLD_HEIGHT-50,
     accelX: 0,
-    accelY: 0
+    accelY: 0,
+    target: {
+      x: WORLD_WIDTH/2,
+      y: WORLD_HEIGHT-50
+    }
   },
   boulders: [
     { x: Math.floor(Math.random() * WORLD_WIDTH), y: 0},
@@ -61,10 +66,15 @@ function moveBoulders() {
 
 function movePlayer() {
   var player = world.player
-  player.x += player.accelX
-  player.y += player.accelY
-  if (player.x+10 > world.width || player.x < 0 || player.y+10 > world.height || player.y < 0) {
-    endGame()
+  accelPlayerTowards(player.target.x, player.target.y)
+  var newX = player.x + player.accelX
+  var newY = player.y + player.accelY
+  if (newX + 10 > world.width || newX < 0 || newY+10 > world.height || newY < 0) {
+    player.accelX = 0
+    player.accelY = 0
+  } else {
+    player.x = newX
+    player.y = newY
   }
 }
 
@@ -83,20 +93,54 @@ function gameLoop() {
   }
 }
 
+function accelPlayerTowards(x, y) {
+  var player = world.player
+
+  if (Math.abs(player.x - x) < 10) {
+    player.accelX = 0
+  }
+  if (Math.abs(player.y - y) < 10) {
+    player.accelY = 0
+  }
+
+  if (player.x < x) {
+    player.accelX += 1
+  } else if (player.x > x) {
+    player.accelX -= 1
+  } else {
+    player.accelX = 0
+  }
+
+  if (player.y < y) {
+    player.accelY += 1
+  } else if (player.y > y) {
+    player.accelY -= 1
+  } else {
+    player.accelY = 0
+  }
+
+  if (player.accelX > MAX_ACCEL) {
+    player.accelX = MAX_ACCEL
+  } else if (player.accelX < MAX_ACCEL * -1) {
+    player.accelX = MAX_ACCEL * -1
+  }
+  if (player.accelY > MAX_ACCEL) {
+    player.accelY = MAX_ACCEL
+  } else if (player.accelY < MAX_ACCEL * -1) {
+    player.accelY = MAX_ACCEL * -1
+  }
+}
+
 $('html').keydown(function(e) {
   var player = world.player
-  if (e.key == "ArrowDown") {
-    if (player.accelY < 0) { player.accelY = 0 }
-    player.accelY += 1
-  } else if (e.key == "ArrowUp") {
-    if (player.accelY > 0) { player.accelY = 0 }
-    player.accelY -= 1
-  } else if (e.key == "ArrowLeft") {
-    if (player.accelX > 0) { player.accelX = 0 }
-    player.accelX -= 1
-  } else if (e.key == "ArrowRight") {
-    if (player.accelX < 0) { player.accelX = 0 }
-    player.accelX += 1
+  if (/[a-z]/.test(e.key)) {
+    var coords = keyCoords(e.key)
+    var scaleX = WORLD_WIDTH / 10
+    var scaleY = 100
+    player.target = {
+      x: 100 + (coords.x * scaleX),
+      y: (WORLD_HEIGHT - 300) + (coords.y * scaleY)
+    }
   }
 })
 
